747dfbcca662d9f4b8f6e1450c95e6ca782e2efa
Dave Kanter <moatgoat@hotmail.com>
2018-05-15 16:56:05 -0700

    Minimum example complete.

---
+++ ch03_Functions/ex_Minimum.js
@@ -0,0 +1,11 @@
+/*	"Minimum"
+	Write a function min that takes two arguments and returns their minimum.
+*/
+
+function retMin(thingOne, thingTwo) {
+	if (thingOne < thingTwo) {
+		return thingOne;
+	}
+
+	return thingTwo;
+}
