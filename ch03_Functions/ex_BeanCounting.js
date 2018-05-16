86eb92be4ce0cc3bbb9b5d557ec671e2558d60a8
Dave Kanter <moatgoat@hotmail.com>
2018-05-16 08:32:58 -0700

    Performance check added to the two isEven solutions, ex_BeanCounting complete.

---
+++ ch03_Functions/ex_BeanCounting.js
@@ -0,0 +1,29 @@
+/*
+	Write a function countBs that takes a string as its only argument
+	and returns a number that indicates how many uppercase "B" characters
+	there are in the string.
+
+	Next write a function called countChar that behaves like countBs except
+	it takes a second argument that indicates the character that is
+	to be counted.  
+*/
+
+function countBs(string) {
+	let finalCount = 0;
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] === "B") {
+			finalCount += 1;
+		}
+	}
+	console.log(`There are ${finalCount} B's in the string.`)
+}
+
+function countChar(string, whichChar) {
+	let finalCount = 0;
+	for (let i = 0; i < string.length; i++) {
+		if (string[i] === whichChar) {
+			finalCount += 1;
+		}
+	}
+	console.log(`There are ${finalCount} ${whichChar}'s in the string.`)
+}
