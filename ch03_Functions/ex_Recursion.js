290ed5547238ba7342c85e772adb102eae5a4fbe
Dave Kanter <moatgoat@hotmail.com>
2018-05-16 08:19:15 -0700

    Early morning commit of some untracked files from last night.

--- ch03_Functions/ex_Recursion.js
+++ ch03_Functions/ex_Recursion.js
@@ -11,6 +11,8 @@
 	It should aceept a single whole number amd return a Boolean.
 */
 
+//this one blows the call stack at 418000
+
 function isEven(whichNumber) {
 	if (whichNumber < 0) {
 		whichNumber *= -1;
@@ -23,4 +25,29 @@ function isEven(whichNumber) {
 		return (isEven(whichNumber - 2));
 	}
 
+}
+
+
+
+//I suspect this is much faster.  Need to write speed check!
+
+function isEvenFast(whichNumber) {
+	return !(whichNumber & 1);
+}
+
+function runBoth(whichFunc1, whichFunc2) {
+	//this should call from an array, duh.
+	callFunc(whichFunc1);
+	callFunc(whichFunc2);
+}
+
+function callFunc(whichFunc) {
+	let t0 = performance.now();
+	whichFunc(info);
+	let t1 = performance.now();
+	console.log(`That function call took ${t1 - t0} milliseconds.`);
+}
+
+function someFunc(info) {
+	console.log(`Some funk ${info}`);
 }
