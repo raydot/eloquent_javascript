9c9db6d9ac41af7120a60bab05a0a5e15d72d617
David Kanter <davidkanter@sf-aqt-dkanter.corp.adobe.com>
2018-07-30 14:51:16 -0700

    Finished with chapter 5

--- ch05_HOFs/index.js
+++ ch05_HOFs/index.js
@@ -1,5 +1,6 @@
 require('./scripts.js')
 
+// FILTER
 function filter(array, test) {
 	let passed = [];
 	for (let element of array) {
@@ -10,4 +11,48 @@ function filter(array, test) {
 	return passed;
 }
 
+console.log('\nLiving scripts:');
 console.log(filter(SCRIPTS, script => script.living));
+
+console.log('\nTop to bottom scripts:');
+console.log(SCRIPTS.filter(s => s.direction == "ttb"));
+
+// MAP
+function map(array, transform) {
+	let mapped = [];
+	for (let element of array) {
+		mapped.push(transform(element));
+	}
+	return mapped;
+}
+
+let rtlScripts = SCRIPTS.filter(s => s.direction == "rtl");
+console.log(map(rtlScripts, s => s.name));
+
+// REDUCE
+// basic reduce
+function reduce(array, combine, start) {
+	let current = start;
+	for (let element of array) {
+		current = combine(current, element);
+	}
+	return current;
+}
+
+console.log('\nSimple reduce:');
+console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));
+
+// If the array contains at least one element you don't need a start value
+console.log(reduce([1, 2, 3, 4], (a, b) => a + b));
+
+// Use reduce to find the script with the most characters:
+console.log('\nSlightly more complicated reduce: ');
+function characterCount(script) {
+	return script.ranges.reduce((count, [from, to]) => {
+		return count + (to - from);
+	}, 0);
+}
+
+console.log(SCRIPTS.reduce((a, b) => {
+	return characterCount(a) < characterCount(b) ? b : a;
+}));
