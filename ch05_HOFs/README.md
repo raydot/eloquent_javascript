b763e33fbb862afd45360ee8bd1b8a7b527c53c4
David Kanter <david.kanter@herodigital.com>
2020-06-25 15:53:12 -0700

    Finished with the test part of Chapter 5.

--- ch05_HOFs/README.md
+++ ch05_HOFs/README.md
@@ -3,3 +3,13 @@
 Looks like I originally tried to follow this chapter from HTML and not Node. Leaving that code in place but it's pretty worthless.
 
 "It is a useful skill, in programming, to notice when you are working at too low a level of abstraction."
+
+## Chapter Summary
+
+This chapter covers
+*   forEach (loop over elements in array)
+*   filter (returns new array matching elements that pass predicate function)
+*   map (transforms an array by putting each element through a function)
+*   reduce (combine all elements in an array into a single value)
+*   some (tests whether any element matches a given predicate function)
+*   findIndex (finds the position of the first element that matches a predicate)
--- ch05_HOFs/recognizing_text.js
+++ ch05_HOFs/recognizing_text.js
@@ -16,6 +16,19 @@ function characterScript(code) {
 
 // We have a characterScript function and a way to correctly loop over characters.
 // The next step is to count the characters that belong to each script.
+
+/**
+ *
+ * @param {*} items
+ * @param {*} groupName
+ *
+ * The countBy function expects a collection (anything that can be looped over)
+ * and a function that computes a group name for a given element.  It returns
+ * an array of objects, each of which names a group and tells you the number of
+ * elements that were found within that group.
+ *
+ * findIndex() returns the first item for which the given function returns true.
+ */
 function countBy(items, groupName) {
   let counts = [];
 
@@ -32,3 +45,34 @@ function countBy(items, groupName) {
 }
 
 console.log(countBy([1, 2, 3, 4, 5], (n) => n > 2));
+
+// Returns : [ { name: false, count: 2 }, { name: true, count: 3 } ]
+
+// Using countBy(), we can write the function that tells us which scripts are used
+// in a piece of text:
+
+function textScripts(text) {
+  let scripts = countBy(text, (char) => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : 'none';
+  }).filter(({ name }) => name != 'none');
+
+  let total = scripts.reduce((n, { count }) => n + count, 0);
+  if (total == 0) return 'No scripts found';
+
+  return scripts
+    .map(({ name, count }) => {
+      return `${Math.round((count * 100) / total)}% ${name}`;
+    })
+    .join(', ');
+}
+
+console.log(textScripts('英国的狗说"woofdoggie", 俄罗斯的狗说"тявтявтявтяв"'));
+
+// Returns: 33% Han, 30% Latin, 36% Cyrillic...COOL!
+
+/**
+ * The function first counts the characters by name, using characterScript to assign
+ * them a name and falling back to "none" for characters that aren't a part of any
+ * script.
+ */
