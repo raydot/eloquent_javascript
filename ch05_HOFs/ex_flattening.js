7497bae3af5678df4021b08bb76102fb5be055e0
Dave Kanter <moatgoat@hotmail.com>
2020-06-26 09:18:38 -0700

    Solved the flattening problem.

--- ch05_HOFs/ex_flattening.js
+++ ch05_HOFs/ex_flattening.js
@@ -5,7 +5,7 @@
  * original arrays.
  */
 
-const { concat } = require('./scripts');
+const { concat, reduce } = require('./scripts');
 
 // EXAMPLE:
 
@@ -14,11 +14,29 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // Your code here.
 
+/*
 const flattenArrays = (arrays) => {
   let outArray = [];
-  const reducer = (ac, currentValue) => outArray.push(currentValue);
-  arrays.reduce(reducer);
+  reduce((arrays, index, outArray) => (outArray = concat(arrays[index])));
+};
+
+flattenArrays(arrays);
+*/
+
+// BRUTE FORCE: This flattens, but not using reduce.
+const flattenArrays = (arrays) => {
+  let outArray = [];
+  for (let element of arrays) {
+    outArray = outArray.concat(element);
+  }
   console.log(outArray);
 };
 
 flattenArrays(arrays);
+
+// THE ANSWER cried the finx:
+const flattenArrays2 = (arrays) => {
+  return arrays.reduce((outArray, nextItem) => outArray.concat(nextItem));
+};
+
+console.log('FA2:', flattenArrays2(arrays));
